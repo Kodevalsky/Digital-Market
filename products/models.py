46db155654a778169bec32fec24710b7c1551bd0
Jeremi Kowalski <jeremikow@gmail.com>
2023-10-06 18:35:01 +0200

    Added searchbar. Added delete button on cart items. Have to implement proper functions

--- products/models.py
+++ products/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=120) #max length is required
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=False, null=True)
     price = models.DecimalField("", max_digits=5, decimal_places=2)
-    summary = models.TextField()
+    summary = models.TextField(max_length=75, blank=False)
     is_preorder = models.BooleanField(default=False)
     photo_name = models.TextField(null=False, default="placeholder")
 
